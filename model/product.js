a85722807246ba81c5bc6826e1929258cfd30844
Ron Barrantes <ronbarrantes@gmail.com>
2017-11-13 14:07:13 -0800

    product model added
    product.js

---
+++ model/product.js
@@ -0,0 +1,19 @@
+'use strict'
+
+const mongoose = require('mongoose')
+
+const productSchema = mongoose.Schema({
+
+  name: { type: String, required: true },
+  category: { type: String },
+  price: { type: Number },
+  image: { type: String },
+  vegan: { type: Boolean },
+  vegetarian: { type: Boolean },
+  glutenFree: { type: Boolean },
+  available: { type: Boolean },
+  created: { type: Date, default: () => new Date() },
+  store: { type: mongoose.Schema.Types.ObjectId, required: true, ref: 'store' },
+})
+
+module.exports = mongoose.model('product', productSchema)
