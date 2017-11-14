c835d002e38ad812efde7f9940aa435c7ac2812e
KatherineHanson <katherinemariehanson@gmail.com>
2017-11-13 16:05:15 -0800

    POST, GET, PUT tests done for Employee router

--- route/employee-router.js
+++ route/employee-router.js
@@ -4,7 +4,6 @@ const { Router } = require('express')
 const httpErrors = require('http-errors')
 const bearerAuth = require('../lib/bearer-auth-middleware.js')
 const Employee = require('../model/employee.js')
-const Store = require('../model/store.js')
 
 let fuzzy = (filterTerm) => new RegExp('.*' + filterTerm.toLowerCase().split('').join('.*') + '.*')
 
@@ -13,28 +12,9 @@ module.exports = new Router()
     console.log('req.body: ', req.body)
     return new Employee({
       ...req.body,
-      photo: undefined,
-      account: req.account._id,
-      username: req.account.username,
-      email: req.account.email,
+      store: req.store._id,
     }).save()
       .then(employee => {
-        res.json(employee)
-      })
-      .catch(next)
-  })
-  .get('/employees/me', bearerAuth, (req, res, next) => {
-    Employee.findOne({account: req.account._id})
-      .then(employee => {
-        res.json(employee)
-      })
-      .catch(next)
-  })
-  .get('/employees/:id', bearerAuth, (req, res, next) => {
-    Employee.findById(req.params.id)
-      .then(employee => {
-        if (!employee)
-          throw httpErrors(404, '__REQUEST_ERROR__ employee not found')
         res.json(employee)
       })
       .catch(next)
@@ -49,8 +29,7 @@ module.exports = new Router()
 
     if (req.query.firstName) req.query.firstName = ({$regex: fuzzy(req.query.firstName), $options: 'i'})
     if (req.query.lastName) req.query.lastName = ({$regex: fuzzy(req.query.lastName), $options: 'i'})
-    if (req.query.city) req.query.city = ({$regex: fuzzy(req.query.city), $options: 'i'})
-    if (req.query.state) req.query.state = ({$regex: fuzzy(req.query.state), $options: 'i'})
+    if (req.query.title) req.query.title = ({$regex: fuzzy(req.query.title), $options: 'i'})
 
     let employeesCache
     Employee.find(req.query)
@@ -76,15 +55,17 @@ module.exports = new Router()
       })
       .catch(next)
   })
-
   .put('/employees/:id', bearerAuth, (req, res, next) => {
-    if (!req.body.firstName || !req.body.lastName)
-      return next(httpErrors(400, 'first name and last name required'))
     Employee.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
       .then(employee => {
         if (!employee)
           throw httpErrors(404, '__REQUEST_ERROR__ employee not found')
         res.json(employee)
       })
+      .catch(next)
+  })
+  .delete('/employees/:id', bearerAuth, (req, res, next) => {
+    Employee.findByIdAndRemove(req.params.id)
+      .then(() => res.sendStatus(204))
       .catch(next)
   })
